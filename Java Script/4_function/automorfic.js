11d7cb15b85d773fbeb2bab0e31098afb3ca79db
Meenakshi Sundaram <meenakshisundaram32982@gmail.com>
2026-08-03 13:13:18 +0530

    update

--- Java Script/4_function/automorfic.js
+++ Java Script/4_function/automorfic.js
@@ -14,3 +14,37 @@ let num = function (num) {
 };
 
 console.log(num(25));
+
+let outer = () => {
+  console.log("i am outer function");
+  let a = 10;
+  let inner = () => {
+    let b = 20;
+    console.log("i am inner function");
+    console.log("a value is ", a);
+    console.log("b value is", b);
+  };
+  // console.log(b); cannot access the variable out side the function
+  inner();
+};
+outer();
+
+
+console.log("****higher orderfunction****");
+
+
+let wish=()=>{
+  console.log("happy birthday");
+  
+}
+
+let greetings = (myFunc)=>{
+  myFunc()
+}
+
+greetings(wish)
+
+
+
+
+
