1851cd3d0f30c84f1116f064931f0a86d1eb1a46
Meenakshi Sundaram <meenakshisundaram32982@gmail.com>
2026-08-04 13:24:11 +0530

    update

--- Java Script/4_function/automorfic.js
+++ Java Script/4_function/automorfic.js
@@ -29,22 +29,82 @@ let outer = () => {
 };
 outer();
 
-
 console.log("****higher orderfunction****");
 
-
-let wish=()=>{
+let wish = () => {
   console.log("happy birthday");
-  
-}
+};
 
-let greetings = (myFunc)=>{
-  myFunc()
-}
+let greetings = (myFunc) => {
+  myFunc();
+};
 
-greetings(wish)
+greetings(wish);
 
+greetings(() => {
+  console.log("i am callback function");
+});
 
+// example of Higherorder and callback function
 
+let add = (a, b) => {
+  console.log("addition is ", a + b);
+};
+let sub = (a, b) => {
+  console.log("subtraction is ", a - b);
+};
+let mul = (a, b) => {
+  console.log("multiplication is ", a * b);
+};
+let division = (a, b) => {
+  console.log("division is ", a / b);
+};
 
+let calculator = (task, num1, num2) => {
+  task(num1, num2);
+};
 
+calculator(add, 10, 39);
+calculator(sub, 100, 30);
+calculator(mul, 19, 21);
+calculator(
+  division,
+  49,
+  7,
+)(
+  // {
+  //   var x=10;
+  //   let y=20;
+  //   const z=30;
+  //   console.log(x);
+  //   console.log(y);
+  //   console.log(z);
+  // }
+  //   console.log(x);
+  //   console.log(y);
+  //   console.log(z);
+
+  // function scope() {
+  //   for (var i = 0; i <= 5; i++) {
+
+  //   }
+  //  console.log(i);
+
+  // }
+
+  // scope();
+
+  // console.log(x); //undefined
+  // var x;
+
+  // console.log(y);//undefined
+  // var y;
+
+  function () {
+    console.log("database connected");
+  },
+)();
+
+(function (port) {
+  console.log("server is running on port number", port);
+})(3000);
