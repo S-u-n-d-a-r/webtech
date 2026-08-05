117930325227f4fbfe0e69819cc8509cb12472bc
Meenakshi Sundaram <meenakshisundaram32982@gmail.com>
2026-08-05 13:34:26 +0530

    update

---
+++ Java Script/5_String/string.js
@@ -0,0 +1,25 @@
+let str1 = "hi";
+let str2 = "hi";
+let str3 = `hi
+hello 
+how 
+are 
+you`;
+
+console.log(typeof str1);
+console.log(typeof str2);
+console.log(typeof str3);
+
+console.log(str3);
+
+let a = 10;
+let b = 20;
+
+let myname = "Soonapana";
+
+console.log(`the addition of ${a} and ${b} is :${a + b}`);
+
+console.log(`my name is : ${myname}`);
+
+let msg = "how are you";
+console.log(msg.length);
